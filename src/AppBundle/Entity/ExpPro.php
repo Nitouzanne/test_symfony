abc1b048188a7d824a68fb0b5c0d0c815828ff8d
Nitouzanne <nicolas.touzanne@gmail.com>
2018-03-12 13:56:53 +0100

    commit phpdoc

--- src/AppBundle/Entity/ExpPro.php
+++ src/AppBundle/Entity/ExpPro.php
@@ -1,41 +1,88 @@
 <?php
 namespace AppBundle\Entity;
-
+/**
+ * Class ExpPro
+ * @author Nicolas Touzanne
+ * @package AppBundle\Entity
+ */
 class ExpPro
 {
+    /**
+     * @var string
+     */
     private $title;
+    /**
+     * @var \datetime object
+     */
     private $dateDeb;
+    /**
+     * @var \datetime object
+     */
     private $dateFin;
+    /**
+     * @var string
+     */
     private $mission;
 
+    /**Getter pour le titre de l'experience professionnel
+     * @return string
+     */
     public function getTitle(){
         return $this->title;
     }
 
+    /** Setter pour le titre de l'expereience pro
+     * @param $titre
+     * @return string
+     */
     public function setTitle($titre){
         $this->title = $titre;
         return $this->title;
     }
 
+    /** Getter pour la date de debut d'expe pro
+     * @return \datetime
+     */
     public function getDateDeb(){
         return $this->dateDeb;
     }
+
+    /** Setter pour la date de debut d'expe pro
+     * @param $date
+     * @return \datetime
+     */
     public function setDateDeb($date){
         $this->dateDeb = $date;
         return $this->dateDeb;
     }
 
+    /** Getter pour la date de debut de fin d'expe pro
+     * @return \datetime
+     */
     public function getDateFin(){
         return $this->dateFin;
     }
+
+    /** Setter pour la date de fin d'expe pro
+     * @param $dateF
+     * @return \datetime
+     */
     public function setDateFin ($dateF){
         $this->dateFin = $dateF;
         return $this->dateFin;
     }
 
+    /** Getter pour la mission de l'expe pro
+     * @return string
+     */
     public function getMission(){
         return $this->mission;
     }
+
+    /** Setter pour la mission de l'expe pro
+     * @param $mission
+     * @return string
+     */
     public function setMission($mission){
         $this->mission = $mission;
         return $this->mission;
--- src/AppBundle/Entity/Formation.php
+++ src/AppBundle/Entity/Formation.php
@@ -9,7 +9,7 @@ class Formation
 {
     /* Titre de la formation
      *
-     * @var string $title.
+     * @var string
      */
     private $title;
 
@@ -27,13 +27,13 @@ class Formation
 
     /* Description de la formation
      *
-     * @var string $Description.
+     * @var string
      */
     private $Description;
 
     /* Resultat de la formation
      *
-     * @var string $diplomeObtenu.
+     * @var string
      */
     private $diplomeObtenu;
 
@@ -45,38 +45,73 @@ class Formation
         return $this->title;
     }
 
+    /** Setter du Titre de la formation
+     * @param $titre
+     * @return string
+     */
     public function setTitle($titre){
         $this->title = $titre;
         return $this->title;
     }
 
+    /** Getter pour datedeb
+     * @return \dateTime object
+     */
     public function getDateDeb(){
         return $this->dateDeb;
     }
+
+    /** Setter pour dateDeb
+     * @param $date
+     * @return \dateTime object
+     */
     public function setDateDeb($date){
         $this->dateDeb = $date;
         return $this->dateDeb;
     }
 
+    /** Getter pour datefin
+     * @return \datetime object
+     */
     public function getDateFin(){
         return $this->dateFin;
     }
+    /** Setteer pour date de fin de la formation
+     * @param $dateF
+     * @return \datetime object
+     */
     public function setDateFin ($dateF){
         $this->dateFin = $dateF;
         return $this->dateFin;
     }
 
+    /** Getter pour Description de la formation
+     * @return string
+     */
     public function getDescription(){
         return $this->Description;
     }
+
+    /**Settern pour la description de la formation
+     * @param $descript
+     * @return string
+     */
     public function setDescription($descript){
         $this->Description = $descript;
         return $this->Description;
     }
 
+    /** Getter pour le resultat de la formation
+     * @return string
+     */
     public function getDiplomeObtenu(){
         return $this->diplomeObtenu;
     }
+
+    /** Setter pour le resultat de la formation
+     * @param $diplobt
+     * @return string
+     */
     public function setDiplomeObtenu($diplobt){
         $this->diplomeObtenu = $diplobt;
         return $this->diplomeObtenu;
